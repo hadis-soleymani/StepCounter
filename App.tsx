5671d51b9d7aa2b3edff98cbbf1754cf07e60d3c
hadis_soleymani <hadiissoleymani@gmail.com>
2023-01-27 12:16:14 -0800

    fix ios map warning

--- App.tsx
+++ App.tsx
@@ -24,7 +24,6 @@
 //   );
 // }
 
-
 // const Stack = createNativeStackNavigator();
 
 // function App() {
@@ -38,29 +37,30 @@
 //   );
 // }
 
-
 // export default App;
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import MapboxGL from "@rnmapbox/maps";
-MapboxGL.setWellKnownTileServer('Mapbox');// solve http error
-MapboxGL.setAccessToken("pk.eyJ1IjoiaGFkaXNzb2xleW1hbmkiLCJhIjoiY2t5d29qbGprMGFqZzJ3cDhnazlhdzR2aCJ9.TYKzTiV2hw91qmw87O4Qow");
+import React, {Component} from 'react';
+import {StyleSheet, View, Platform} from 'react-native';
+import MapboxGL from '@rnmapbox/maps';
+Platform.OS === 'android' && MapboxGL.setWellKnownTileServer('Mapbox'); // solve http error
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiaGFkaXNzb2xleW1hbmkiLCJhIjoiY2t5d29qbGprMGFqZzJ3cDhnazlhdzR2aCJ9.TYKzTiV2hw91qmw87O4Qow',
+);
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   container: {
-    height: 300,
     width: 300,
-    backgroundColor: "tomato"
+    height: 300,
+    backgroundColor: 'tomato',
   },
   map: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
 export default class App extends Component {
